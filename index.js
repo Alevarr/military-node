444be403ff143945728f496fe8b7a65681167808
Alevar <chupick228@gmail.com>
2024-02-10 18:25:01 +0300

    feat: add get single citizen route

--- index.js
+++ index.js
@@ -40,6 +40,45 @@ app.get("/api/citizens", (req, res) => {
   );
 });
 
+app.get("/api/citizens/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query(
+    `SELECT  
+    citizens.id,
+    citizens.first_name,
+    citizens.middle_name,
+    citizens.last_name,
+    citizens.passport,
+    ARRAY_AGG(DISTINCT jsonb_build_object('release_date', militaries.release_date, 'military_serial', militaries.military_serial, 'comment', militaries.comment)) AS militaries,
+    personal_files.feasibility_category,
+    personal_files.deferment_end_date,
+    ARRAY_AGG(DISTINCT jsonb_build_object('type', record_history.type, 'date', record_history.date, 'department', jsonb_build_object('name', departments.name, 'address', departments.address))) AS records
+  FROM  
+    citizens
+  LEFT JOIN  
+    militaries ON citizens.id = militaries.citizen_id
+  LEFT JOIN   
+  personal_files ON citizens.personal_file_id = personal_files.id
+  LEFT JOIN   
+    record_history ON personal_files.id = record_history.personal_file_id
+  LEFT JOIN   
+  departments ON record_history.department_id = departments.id
+  WHERE
+	citizens.id = ${id}
+  GROUP BY
+   citizens.id,
+   personal_files.feasibility_category,
+   personal_files.deferment_end_date
+  `,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.send(results.rows[0]);
+    }
+  );
+});
+
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
