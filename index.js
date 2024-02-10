99a32da0fa081fe936180387cb10ba18486bcb9f
Alevar <chupick228@gmail.com>
2024-02-10 16:18:51 +0300

    feat: add get citizens route

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const config = require("config");
+const cors = require("cors");
 const express = require("express");
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 
 const app = express();
 const pool = new Pool({
-  user: config.get('user'),
-  host: config.get('host'),
-  database: config.get('database'),
-  password: config.get('password'),
-  port: config.get('db_port'),
-})
+  user: config.get("user"),
+  host: config.get("host"),
+  database: config.get("database"),
+  password: config.get("password"),
+  port: config.get("db_port"),
+});
 
-console.log(pool)
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("<h1>Api is running!</h1>");
-  });
+  res.send("<h1>Api is running!</h1>");
+});
 
-app.get('/api/users', (req, res) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-        if (error) {
-        throw error
-        }
-        res.status(200).json(results.rows)
-    })
-})
+app.get("/api/users", (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.send(results.rows);
+  });
+});
+
+app.get("/api/citizens", (req, res) => {
+  pool.query(
+    "SELECT citizens.id, citizens.first_name, citizens.middle_name, citizens.last_name, citizens.passport, personal_files.feasibility_category, personal_files.deferment_end_date FROM citizens JOIN personal_files ON citizens.personal_file_id = personal_files.id;",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.send(results.rows);
+    }
+  );
+});
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
